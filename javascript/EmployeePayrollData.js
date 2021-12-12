dd2428da01489b2af7a90b1ea0284d08465d37ae
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-12-12 19:17:28 +0530

    [VIGNESH M] Added: added modified for  name validation

--- javascript/EmployeePayrollData.js
+++ javascript/EmployeePayrollData.js
@@ -1,61 +1,48 @@
 //UC9-On form submit populate the employee payroll data
 class EmployeePayrollData {
-    get name() {
-        return this._name;
+
+    get id(){ return this._id; }
+    set id(id){
+        this._id=id;
     }
 
+    get name() { return this._name; }
     set name(name) {
-        this._name = name;
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$')
+        if(nameRegex.test(name))
+           this.name =name;
+        else throw 'Name is incorrect';
     }
 
-    get profilePic() {
-        return this._profilePic;
-    }
-
+    get profilePic() { return this._profilePic; }
     set profilePic(profilePic) {
         this._profilePic = profilePic;
     }
 
-    get gender() {
-        return this._gender;
-    }
-
+    get gender() { return this._gender; }
     set gender(gender) {
         this._gender = gender;
     }
 
-    get department() {
-        return this._department;
-    }
-
+    get department() { return this._department; }
     set department(department) {
         this._department = department;
     }
 
-    get salary() {
-        return this._salary;
-    }
-
+    get salary() { return this._salary; }
     set salary(salary) {
         this._salary = salary;
     }
     
-    get start_date() {
-        return this._start_date;
-    }
-
+    get start_date() { return this._start_date; }
     set start_date(start_date) {
         this._start_date = start_date;
     }
 
-    get note() {
-        return this._note;
-    }
-
+    get note() { return this._note; }
     set note(note) {
         this._note = note;
     }
-
     
     toString() 
     {
