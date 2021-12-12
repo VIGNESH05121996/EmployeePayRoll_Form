46ec9bfaf9e85c193c2978ecc2e8cf5b88b5433c
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-12-12 13:25:48 +0530

    [VIGNESH M] Added: added name validation and other parameters getter setter method

--- javascript/Employee.js
+++ javascript/Employee.js
@@ -6,3 +6,13 @@ salary.addEventListener('input', function()
 {
     output.textContent = salary.value;
 });
+
+const text = document.querySelector('#name');
+const  textError=document.querySelector('.text-Error');
+text.addEventListener('input',function()
+    {
+    let nameRegex= RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if(nameRegex.test(text.value))
+    textError.textContent="";
+    else textError.textContent="name is incorrect";
+});
